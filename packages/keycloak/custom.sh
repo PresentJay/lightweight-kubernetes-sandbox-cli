81bf24ba6cc1d8d3597a3c1773f8a99a832c621a
Present_Jay.Dev <Presentj94@gmail.com>
2022-09-20 16:15:17 +0900

    feat(k8s/keyclaok): operator test version 설치 시도 -> resource not found?

--- packages/keycloak/custom.sh
+++ packages/keycloak/custom.sh
@@ -7,42 +7,30 @@ getEnv ./packages/keycloak/.env
 
 case $(checkOpt iu $@) in
     i | install)
-        ! checkHelmRepo ${CHART_REPOSITORY_NAME} && \
-            helm repo add ${CHART_REPOSITORY_NAME} ${CHART_REGISTRY} && helm repo update
-        ! checkNamespace ${INSTALL_NAMESPACE} && \
-            kubectl create namespace ${INSTALL_NAMESPACE}
-        helm upgrade ${INSTALL_NAME} ${CHART_REPOSITORY_ORG}/${CHART_REPOSITORY_NAME} \
-            --version ${INSTALL_VERSION} \
-            --namespace ${INSTALL_NAMESPACE} \
-            --set=extraArgs=${EXTRAARGS} \
-            --install \
-            --no-hooks
-        applyIngressNginxHTTPS ${INGRESS_HOSTNAME} ${INGRESS_SERVICE} ${INGRESS_PORT} ${PACKAGE_LABEL} ${INSTALL_NAMESPACE}
+        kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/19.0.2/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
+        kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/19.0.2/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
+        kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/19.0.2/kubernetes/kubernetes.yml
+        applyIngressNginxHTTP ${INGRESS_HOSTNAME} ${INGRESS_SERVICE} ${INGRESS_PORT} ${PACKAGE_LABEL}
     ;;
     u | uninstall | teardown)
-        deleteSequence ingress ${INSTALL_NAME} ${INSTALL_NAMESPACE}
-        deleteSequence helm ${INSTALL_NAME} ${INSTALL_NAMESPACE}
-        deleteSequence namespace ${INSTALL_NAMESPACE}
-        deleteSequence helm-repo ${CHART_REPOSITORY_NAME}
+        
     ;;
     open)
         if checkParamIsInList ${INGRESS_PROTOCOL} http https; then
             _HOSTURL_="${INGRESS_HOSTNAME}.${masterNodeIP}.nip.io"
-            _PORT_=$(bash packages/ingress-nginx/helm.sh --httpsPort)
+            _PORT_=$(bash packages/ingress-nginx/helm.sh --${INGRESS_PROTOCOL}Port)
             _OPENURI_="${INGRESS_PROTOCOL}://${_HOSTURL_}:${_PORT_}"
             openURI ${_OPENURI_}
-            logInfo "fetching dashboard-k8s login token . . ."
-            echo $(bash packages/kubernetes-dashboard/helm.sh --token) 
         else
             logKill "please set INGRESS_PROTOCOL to http or https only. (in .env)"
         fi
     ;;
     h | help | ? | *)
-        logHelpHead "packages/kubernetes-dashboard/helm.sh"
-        logHelpContent i install "install kubernetes-dashboard package"
-        logHelpContent u uninstall "uninstall kubernetes-dashboard package"
-        logHelpContent open "open kubernetes-dashboard web"
-        logHelpContent token "get kubernetes-dashboard admin token"
+        logHelpHead "packages/keycloak/custom.sh"
+        logHelpContent i install "install keycloak package"
+        logHelpContent u uninstall "uninstall keycloak package"
+        logHelpContent open "open keycloak web"
+        logHelpContent token "get keycloak admin token"
         logHelpTail
     ;;
 esac
