bcef0cd842927f70b9d8a3095aa0f845e8cfa2e8
Present_Jay.Dev <Presentj94@gmail.com>
2022-09-26 17:07:46 +0900

    feat: wiki js 설치

---
+++ packages/wiki.js/helm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Author: PresentJay (정현재, presentj94@gmail.com)
+
+source ./scripts/kubecheats.sh
+getEnv ./packages/wiki.js/.env
+
+case $(checkOpt iu $@) in
+    i | install)
+        ! checkHelmRepo ${CHART_REPOSITORY_NAME} && \
+            helm repo add ${CHART_REPOSITORY_NAME} ${CHART_REGISTRY} && helm repo update
+        ! checkNamespace ${INSTALL_NAMESPACE} && \
+            kubectl create namespace ${INSTALL_NAMESPACE}
+        helm upgrade ${INSTALL_NAME} ${CHART_ORG}/${CHART_NAME} \
+            --namespace ${INSTALL_NAMESPACE} \
+            --install \
+            --set postgresql.persistence.storageClass=${STORAGE} \
+            --set postgresql.persistence.size=${PVC_SIZE}
+        applyIngressNginxHTTP ${INGRESS_HOSTNAME} ${INGRESS_SERVICE} ${INGRESS_PORT} ${PACKAGE_LABEL} ${INSTALL_NAMESPACE}
+    ;;
+    u | uninstall | teardown)
+        deleteSequence helm ${INSTALL_NAME} ${INSTALL_NAMESPACE}
+        deleteSequence namespace ${INSTALL_NAMESPACE}
+        deleteSequence helm-repo ${CHART_REPOSITORY_NAME}
+    ;;
+    open)
+        if checkParamIsInList ${INGRESS_PROTOCOL} http https; then
+            _HOSTURL_="${INGRESS_HOSTNAME}.${masterNodeIP}.nip.io"
+            _PORT_=$(bash packages/ingress-nginx/helm.sh --${INGRESS_PROTOCOL}Port)
+            _OPENURI_="${INGRESS_PROTOCOL}://${_HOSTURL_}:${_PORT_}"
+            openURI ${_OPENURI_}
+        else
+            logKill "please set INGRESS_PROTOCOL to http or https only. (in .env)"
+        fi
+    ;;
+    h | help | ? | *)
+        logHelpHead "packages/wiki/helm.sh"
+        logHelpContent i install "install wiki package"
+        logHelpContent u uninstall "uninstall wiki package"
+        logHelpTail
+    ;;
+esac
